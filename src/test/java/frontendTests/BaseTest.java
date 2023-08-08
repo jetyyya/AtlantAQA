0a3d0992ca6f5cbc438d981af7086800ca782a6f
jetyyya <jetyyya@gmail.com>
2023-08-08 18:11:09 +0300

    Changing pom

--- src/test/java/frontendTests/BaseTest.java
+++ src/test/java/frontendTests/BaseTest.java
@@ -2,22 +2,22 @@ package frontendTests;
 
 import frontend.core.DriverSingleton;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
 
 public abstract class BaseTest {
     protected WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() {
         driver = DriverSingleton.getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)).pageLoadTimeout(Duration.ofSeconds(10));
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         DriverSingleton.quitDriver();
     }
