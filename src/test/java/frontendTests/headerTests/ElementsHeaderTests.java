1bd044a7ebccb3efbcee1f06372fda71d98a7353
jetyyya <jetyyya@gmail.com>
2023-08-12 15:41:59 +0300

    Changing header tests

--- src/test/java/frontendTests/headerTests/ElementsHeaderTests.java
+++ src/test/java/frontendTests/headerTests/ElementsHeaderTests.java
@@ -2,6 +2,7 @@ package frontendTests.headerTests;
 
 import frontend.blogPage.ElementsBlogPage;
 import frontend.contactsPage.ElementsContactsPage;
+import frontend.core.waiters.WaiterVisibilityOf;
 import frontend.deliveryPage.ElementsDeliveryPage;
 import frontend.header.ElementsHeader;
 import frontend.mainPage.ElementsMainPage;
@@ -24,31 +25,43 @@ public class ElementsHeaderTests extends BaseTest {
     @Test
     public void vacancyPageLoading() {
         elementsHeader.getVacancy().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsVacancyPage.getTitle());
         assertTrue(elementsVacancyPage.getTitle().isDisplayed(), "The 'Vacancy' page did not load");
     }
     @Test
     public void blogPageLoading() {
         elementsHeader.getBlog().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsBlogPage.getTitle());
         assertTrue(elementsBlogPage.getTitle().isDisplayed(), "The 'Blog' page did not load");
     }
     @Test
     public void deliveryPageLoading() {
         elementsHeader.getDelivery().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsDeliveryPage.getTitle());
         assertTrue(elementsDeliveryPage.getTitle().isDisplayed(), "The 'Delivery' page did not load");
     }
     @Test
     public void reviewPageLoading() {
         elementsHeader.getReview().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsReviewPage.getTitle());
         assertTrue(elementsReviewPage.getTitle().isDisplayed(), "The 'Review' page did not load");
     }
     @Test
     public void contactsPageLoading() {
         elementsHeader.getContacts().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsContactsPage.getTitle());
         assertTrue(elementsContactsPage.getTitle().isDisplayed(), "The 'Contacts' page did not load");
     }
     @Test
     public void discountPageLoading() {
         elementsMainPage.getRegionSwitchList().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsMainPage.getRegionSwitchListTitle());
         assertTrue(elementsMainPage.getRegionSwitchListTitle().isDisplayed(), "Region switch menu not displayed");
     }
 }
