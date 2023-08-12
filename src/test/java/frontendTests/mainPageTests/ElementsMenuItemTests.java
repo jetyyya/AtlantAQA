6b148a4e24da30e3f1e2802c8c84fbe72d9ccc05
jetyyya <jetyyya@gmail.com>
2023-08-12 15:33:19 +0300

    Changing menu-item tests

--- src/test/java/frontendTests/mainPageTests/ElementsMenuItemTests.java
+++ src/test/java/frontendTests/mainPageTests/ElementsMenuItemTests.java
@@ -1,5 +1,6 @@
 package frontendTests.mainPageTests;
 
+import frontend.core.waiters.WaiterVisibilityOf;
 import frontend.discountPage.ElementsDiscountPage;
 import frontend.loanPage.ElementsLoanPage;
 import frontend.mainPage.ElementsMenuItem;
@@ -20,21 +21,29 @@ public class ElementsMenuItemTests extends BaseTest {
     @Test
     public void discountPageLoading() {
         elementsMenuItem.getDiscount().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsDiscountPage.getTitle());
         assertTrue(elementsDiscountPage.getTitle().isDisplayed(), "The 'Promotions' page did not load");
     }
     @Test
     public void retailPageLoading() {
         elementsMenuItem.getRetail().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsRetailPage.getTitle());
         assertTrue(elementsRetailPage.getTitle().isDisplayed(), "The 'Retail' page did not load");
     }
     @Test
     public void wholesalePageLoading() {
         elementsMenuItem.getWholesale().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsWholesalePage.getTitle());
         assertTrue(elementsWholesalePage.getTitle().isDisplayed(), "The 'Wholesale' page did not load");
     }
     @Test
     public void loanPageLoading() {
         elementsMenuItem.getLoan().click();
+        WaiterVisibilityOf waiterVisibilityOfTitle = new WaiterVisibilityOf(driver);
+        waiterVisibilityOfTitle.waitForVisibility(elementsLoanPage.getTitle());
         assertTrue(elementsLoanPage.getTitle().isDisplayed(), "The 'Loan' page did not load");
     }
 }
